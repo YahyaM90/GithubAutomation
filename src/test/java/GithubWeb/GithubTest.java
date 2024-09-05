b969202a25d33e926ef10c5120f2b057d82d67bd
Yahyamt <y.m.t.070@hotmail.com>
2024-09-05 20:05:07 +0300

    First commit from second branch

--- src/test/java/GithubWeb/GithubTest.java
+++ src/test/java/GithubWeb/GithubTest.java
@@ -2,6 +2,8 @@ package GithubWeb;
 
 import Base.BaseClass;
 import Pages.GithubPages;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.Test;
 
 public class GithubTest extends BaseClass {
@@ -9,6 +11,7 @@ public class GithubTest extends BaseClass {
 
 
     @Test(testName = "TS-001")
+    @Severity(SeverityLevel.BLOCKER)
     public void ClkdocMethod() throws InterruptedException {
         GithubPages Docs = new GithubPages(driver);
 
